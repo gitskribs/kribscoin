2d1c5ba766a08b87e5f55b0a0d3fee78c687361e
gitskribs <37917839+gitskribs@users.noreply.github.com>
2018-04-16 19:51:13 +0000

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kribi/Documents/Logiciels/Cryptonote/src
+# Build directory: /home/kribi/Documents/Logiciels/Cryptonote/build/release/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
